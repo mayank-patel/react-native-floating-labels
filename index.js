e330040c062b1f6c11fde69656d3e802ac7abbd8
Rahul Purohit <rahul@punchh.com>
2018-02-08 13:48:31 +0530

    Added Props dirtyStyle and cleanStyle

--- index.js
+++ index.js
@@ -1,209 +1,217 @@
 'use strict';
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import {
-  StyleSheet,
-  TextInput,
-  LayoutAnimation,
-  Animated,
-  Easing,
-  Text,
-  View,
-  Platform
+    StyleSheet,
+    TextInput,
+    LayoutAnimation,
+    Animated,
+    Easing,
+    Text,
+    View,
+    Platform
 } from 'react-native';
 
-var textPropTypes = Text.propTypes || View.propTypes
-var textInputPropTypes = TextInput.propTypes || textPropTypes
+var textPropTypes = Text.propTypes || View.propTypes;
+var textInputPropTypes = TextInput.propTypes || textPropTypes;
 var propTypes = {
-  ...textInputPropTypes,
-  inputStyle: textInputPropTypes.style,
-  labelStyle: textPropTypes.style,
-  disabled: PropTypes.bool,
-  style: View.propTypes.style,
-}
+    ...textInputPropTypes,
+    inputStyle: textInputPropTypes.style,
+    labelStyle: textPropTypes.style,
+    disabled: PropTypes.bool,
+    style: View.propTypes.style,
+    dirtyStyle: View.propTypes.style,
+    cleanStyle: View.propTypes.style
+};
 
-var FloatingLabel  = React.createClass({
-  propTypes: propTypes,
+var FloatingLabel = React.createClass({
+    propTypes: propTypes,
 
-  getInitialState () {
-    var state = {
-      text: this.props.value,
-      dirty: (this.props.value || this.props.placeholder)
-    };
+    getInitialState() {
+        var state = {
+            text: this.props.value,
+            dirty: this.props.value || this.props.placeholder
+        };
+        dirtyStyle = this.props.dirtyStyle || dirtyStyle;
+        cleanStyle = this.props.cleanStyle || cleanStyle;
 
-    var style = state.dirty ? dirtyStyle : cleanStyle
-    state.labelStyle = {
-      fontSize: new Animated.Value(style.fontSize),
-      top: new Animated.Value(style.top)
+        var style = state.dirty ? dirtyStyle : cleanStyle;
+        state.labelStyle = {
+            fontSize: new Animated.Value(style.fontSize),
+            top: new Animated.Value(style.top)
+        };
+
+        return state;
+    },
+
+    componentWillReceiveProps(props) {
+        if (
+            typeof props.value !== 'undefined' &&
+            props.value !== this.state.text
+        ) {
+            this.setState({ text: props.value, dirty: !!props.value });
+            this._animate(!!props.value);
+        }
+    },
+
+    _animate(dirty) {
+        var nextStyle = dirty ? dirtyStyle : cleanStyle;
+        var labelStyle = this.state.labelStyle;
+        var anims = Object.keys(nextStyle).map(prop => {
+            return Animated.timing(
+                labelStyle[prop],
+                {
+                    toValue: nextStyle[prop],
+                    duration: 200
+                },
+                Easing.ease
+            );
+        });
+
+        Animated.parallel(anims).start();
+    },
+
+    _onFocus() {
+        this._animate(true);
+        this.setState({ dirty: true });
+        if (this.props.onFocus) {
+            this.props.onFocus(arguments);
+        }
+    },
+
+    _onBlur() {
+        if (!this.state.text) {
+            this._animate(false);
+            this.setState({ dirty: false });
+        }
+
+        if (this.props.onBlur) {
+            this.props.onBlur(arguments);
+        }
+    },
+
+    onChangeText(text) {
+        this.setState({ text });
+        if (this.props.onChangeText) {
+            this.props.onChangeText(text);
+        }
+    },
+
+    updateText(event) {
+        var text = event.nativeEvent.text;
+        this.setState({ text });
+
+        if (this.props.onEndEditing) {
+            this.props.onEndEditing(event);
+        }
+    },
+
+    _renderLabel() {
+        return (
+            <Animated.Text
+                ref="label"
+                style={[
+                    this.state.labelStyle,
+                    styles.label,
+                    this.props.labelStyle
+                ]}
+            >
+                {this.props.children}
+            </Animated.Text>
+        );
+    },
+
+    render() {
+        var props = {
+                autoCapitalize: this.props.autoCapitalize,
+                autoCorrect: this.props.autoCorrect,
+                autoFocus: this.props.autoFocus,
+                bufferDelay: this.props.bufferDelay,
+                clearButtonMode: this.props.clearButtonMode,
+                clearTextOnFocus: this.props.clearTextOnFocus,
+                controlled: this.props.controlled,
+                editable: this.props.editable,
+                enablesReturnKeyAutomatically: this.props
+                    .enablesReturnKeyAutomatically,
+                keyboardType: this.props.keyboardType,
+                multiline: this.props.multiline,
+                numberOfLines: this.props.numberOfLines,
+                onBlur: this._onBlur,
+                onChange: this.props.onChange,
+                onChangeText: this.onChangeText,
+                onEndEditing: this.updateText,
+                onFocus: this._onFocus,
+                onSubmitEditing: this.props.onSubmitEditing,
+                password: this.props.secureTextEntry || this.props.password, // Compatibility
+                placeholder: this.props.placeholder,
+                secureTextEntry:
+                    this.props.secureTextEntry || this.props.password, // Compatibility
+                returnKeyType: this.props.returnKeyType,
+                selectTextOnFocus: this.props.selectTextOnFocus,
+                selectionState: this.props.selectionState,
+                style: [styles.input],
+                testID: this.props.testID,
+                value: this.state.text,
+                underlineColorAndroid: this.props.underlineColorAndroid, // android TextInput will show the default bottom border
+                onKeyPress: this.props.onKeyPress
+            },
+            elementStyles = [styles.element];
+
+        if (this.props.inputStyle) {
+            props.style.push(this.props.inputStyle);
+        }
+
+        if (this.props.style) {
+            elementStyles.push(this.props.style);
+        }
+
+        return (
+            <View style={elementStyles}>
+                {this._renderLabel()}
+                <TextInput {...props} />
+            </View>
+        );
     }
-
-    return state
-  },
-
-  componentWillReceiveProps (props) {
-    if (typeof props.value !== 'undefined' && props.value !== this.state.text) {
-      this.setState({ text: props.value, dirty: !!props.value })
-      this._animate(!!props.value)
-    }
-  },
-
-  _animate(dirty) {
-    var nextStyle = dirty ? dirtyStyle : cleanStyle
-    var labelStyle = this.state.labelStyle
-    var anims = Object.keys(nextStyle).map(prop => {
-      return Animated.timing(
-        labelStyle[prop],
-        {
-          toValue: nextStyle[prop],
-          duration: 200
-        },
-        Easing.ease
-      )
-    })
-
-    Animated.parallel(anims).start()
-  },
-
-  _onFocus () {
-    this._animate(true)
-    this.setState({dirty: true})
-    if (this.props.onFocus) {
-      this.props.onFocus(arguments);
-    }
-  },
-
-  _onBlur () {
-    if (!this.state.text) {
-      this._animate(false)
-      this.setState({dirty: false});
-    }
-
-    if (this.props.onBlur) {
-      this.props.onBlur(arguments);
-    }
-  },
-
-  onChangeText(text) {
-    this.setState({ text })
-    if (this.props.onChangeText) {
-      this.props.onChangeText(text)
-    }
-  },
-
-  updateText(event) {
-    var text = event.nativeEvent.text
-    this.setState({ text })
-
-    if (this.props.onEndEditing) {
-      this.props.onEndEditing(event)
-    }
-  },
-
-  _renderLabel () {
-    return (
-      <Animated.Text
-        ref='label'
-        numberOfLines={this.props.numberOfLines}
-        style={[this.state.labelStyle, styles.label, this.props.labelStyle]}
-      >
-        {this.props.children}
-      </Animated.Text>
-    )
-  },
-
-  render() {
-    var props = {
-        autoCapitalize: this.props.autoCapitalize,
-        autoCorrect: this.props.autoCorrect,
-        autoFocus: this.props.autoFocus,
-        bufferDelay: this.props.bufferDelay,
-        clearButtonMode: this.props.clearButtonMode,
-        clearTextOnFocus: this.props.clearTextOnFocus,
-        controlled: this.props.controlled,
-        editable: this.props.editable,
-        enablesReturnKeyAutomatically: this.props.enablesReturnKeyAutomatically,
-        keyboardType: this.props.keyboardType,
-        multiline: this.props.multiline,
-        numberOfLines: this.props.numberOfLines,
-        onBlur: this._onBlur,
-        onChange: this.props.onChange,
-        onChangeText: this.onChangeText,
-        onEndEditing: this.updateText,
-        onFocus: this._onFocus,
-        onSubmitEditing: this.props.onSubmitEditing,
-        password: this.props.secureTextEntry || this.props.password, // Compatibility
-        placeholder: this.props.placeholder,
-        secureTextEntry: this.props.secureTextEntry || this.props.password, // Compatibility
-        returnKeyType: this.props.returnKeyType,
-        selectTextOnFocus: this.props.selectTextOnFocus,
-        selectionState: this.props.selectionState,
-        style: [styles.input],
-        testID: this.props.testID,
-        value: this.state.text,
-        underlineColorAndroid: this.props.underlineColorAndroid, // android TextInput will show the default bottom border
-        onKeyPress: this.props.onKeyPress
-      },
-      elementStyles = [styles.element];
-
-    if (this.props.inputStyle) {
-      props.style.push(this.props.inputStyle);
-    }
-
-    if (this.props.style) {
-      elementStyles.push(this.props.style);
-    }
-
-    return (
-  		<View style={elementStyles}>
-        {this._renderLabel()}
-        <TextInput
-          ref={(r) => { this.input = r; }}
-          {...props}
-        >
-        </TextInput>
-      </View>
-    );
-  },
 });
 
 var labelStyleObj = {
-  marginTop: 21,
-  paddingLeft: 9,
-  color: '#AAA',
-  position: 'absolute'
-}
+    marginTop: 21,
+    paddingLeft: 9,
+    color: '#AAA',
+    position: 'absolute'
+};
 
 if (Platform.OS === 'web') {
-  labelStyleObj.pointerEvents = 'none'
+    labelStyleObj.pointerEvents = 'none';
 }
 
 var styles = StyleSheet.create({
-  element: {
-    position: 'relative'
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-    borderWidth: 1,
-    color: 'black',
-    fontSize: 20,
-    borderRadius: 4,
-    paddingLeft: 10,
-    marginTop: 20,
-  },
-  label: labelStyleObj
-})
+    element: {
+        position: 'relative'
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        borderWidth: 1,
+        color: 'black',
+        fontSize: 20,
+        borderRadius: 4,
+        paddingLeft: 10,
+        marginTop: 20
+    },
+    label: labelStyleObj
+});
 
 var cleanStyle = {
-  fontSize: 20,
-  top: 7
-}
+    fontSize: 20,
+    top: 7
+};
 
 var dirtyStyle = {
-  fontSize: 12,
-  top: -17,
-}
+    fontSize: 12,
+    top: -17
+};
 
 module.exports = FloatingLabel;
